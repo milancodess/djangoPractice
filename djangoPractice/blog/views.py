7d1d5c0381819d2a3271c47b79db689936b2af87
milancodess <milanbhandari153@gmail.com>
2025-07-02 20:11:56 +0545

    Practice Day 2

--- djangoPractice/blog/views.py
+++ djangoPractice/blog/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'July 1, 2025'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'July 1, 2025'
-    }
-
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
